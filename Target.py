80f81d605dd330dfdd2ce196c0791745b063a8a6
Mixbo <mixbo@wakowakowako.com>
2015-10-26 21:25:52 -0400

    Url formatting and Version module

--- Target.py
+++ Target.py
@@ -1,3 +1,9 @@
 class Target(object):
 	def __init__(self,url):
-		self.url = url
+		self.url = self.sanitizeUrl(url)
+	def sanitizeUrl(self,url):
+		if "://" not in url: 
+                        url = "http://"+url
+                if url[len(url)-1] != "/":
+                        url = url+"/"
+		return url
--- modules/Version.py
+++ modules/Version.py
@@ -1,12 +1,23 @@
 from pwn import *
+import urllib2
 
 def passive(url):
 	log.info("Trying to get WordPress Version")
-	version = "4.2.3"
-	if version:
-		return ("version",version)
-	else:
-		return ("version",False)
+	url+="readme.html"
+	version = False
+	html = ""
+	try:
+		response = urllib2.urlopen(url)
+		html = response.read()
+		html = html.split('\n')
+	except:
+		log.failure("Wasn't able to read "+url)
+	for line in html:
+		p = line.find('<br /> Version')
+		if p != -1:
+			versionLine = line.split('\x20')
+			version = versionLine[len(versionLine)-1]
+	return ("version",version)
     
 def active(url):
 	return passive(url)
