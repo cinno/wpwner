232180bf627b0a1ed563401f7bcdad94b8b4fa80
Mixbo <mixbo@wakowakowako.com>
2015-10-28 13:03:05 -0400

    Action after modules

--- WPwner.py
+++ WPwner.py
@@ -48,6 +48,7 @@ class WPwner(object):
 		log.info('Starting attack on '+self.target.url)
 		if self.hostUp():
 			self.attack()
+			self.done()
 	def hostUp(self):
 		# Verify host is online
 		return True
@@ -72,5 +73,38 @@ class WPwner(object):
 							setattr(self.target,attribute,[value])
 			else:
 				log.failure('Problem with Module '+module_name)
+	def done(self):
+		print "\n\n"
+		log.info('Execution of modules over.\n Note that every bruteforce modules uses the unmatched passwords as the wordlist\nWhat do you want to do?')
+		i = 1
+		print str(i)+" Show what we got"
+		i += 1
+		if hasattr(self.target,self.c.SSH_Password):
+			print str(i)+" Login over SSH"
+			i += 1
+		if hasattr(self.target,self.c.SSH_User):
+			print str(i)+" Bruteforce password for one user"
+			i+= 1
+		if hasattr(self.target,self.c.User):
+			print str(i)+" Bruteforce SSH with an unmatched user"
+			i += 1
+		if hasattr(self.target,self.c.WP_Password):
+			print str(i)+" Open WordPress Dashboard"
+			i += 1
+			print str(i)+" Upload PHP Shell in templates"
+			i += 1
+		if hasattr(self.target,self.c.WP_User):
+			print str(i)+" Bruteforce dashboard for one user"
+			i += 1
+		if hasattr(self.target,self.c.User):
+			print str(i)+" Bruteforce Dashboard with unmatched user"
+			i += 1
+		if hasattr(self.target,self.c.DB_Host) and hasattr(self.target,self.c.DB_User) and hasattr(self.target,self.c.DB_Password):
+			print str(i)+" Direct connection to database"
+			i += 1
+			print str(i)+" Try the database's credentials on a different service"
+			i += 1
+		print str(i)+" Rerun the modules to use newly found information"
+
 if __name__ == "__main__":
 	main()
